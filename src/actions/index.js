33c2e1e0ab0505353473cf1788e7fa4a50a02812
KotaroSugawara <KoSugawara@mx1.wiseman.co.jp>
2017-11-09 10:36:41 +0900

    Fixed actions to async

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import fetch from 'isomorphic-fetch'
+
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
 export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
 export const REQUEST_POSTS = 'REQUEST_POSTS'
@@ -14,6 +16,18 @@ export function invalidateSubreddit(subreddit) {
   return {
     type: INVALIDATE_SUBREDDIT,
     subreddit
+  }
+}
+
+export function fetchPosts(subreddit) {
+  return (dispatch) {
+    dispatch(requestPost(subreddit))
+
+    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+      .then(res => res.json())
+      .then(json =>
+        dispatch(receivePosts(subreddit, json))
+      )
   }
 }
 
