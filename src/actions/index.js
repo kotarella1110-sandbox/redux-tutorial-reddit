b74058c8e4fb682a902be86a356fc429da1b96c8
KotaroSugawara <KoSugawara@mx1.wiseman.co.jp>
2017-11-13 16:46:10 +0900

    Added middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,3 @@
-import fetch from 'isomorphic-fetch'
-
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
 export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
 export const REQUEST_POSTS = 'REQUEST_POSTS'
@@ -19,19 +17,7 @@ export function invalidateSubreddit(subreddit) {
   }
 }
 
-export function fetchPosts(subreddit) {
-  return (dispatch) {
-    dispatch(requestPost(subreddit))
-
-    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
-      .then(res => res.json())
-      .then(json =>
-        dispatch(receivePosts(subreddit, json))
-      )
-  }
-}
-
-function requestPost(subreddit) {
+function requestPosts(subreddit) {
   return {
     type: REQUEST_POSTS,
     subreddit
@@ -43,6 +29,73 @@ function receivePosts(subreddit, json) {
     type: RECEIVE_POSTS,
     subreddit,
     posts: json.data.children.map(child => child.data),
-    receivedAt: Data.now()
+    receivedAt: Date.now()
+  }
+}
+
+// Meet our first thunk action creator!
+// Though its insides are different, you would use it just like any other action creator:
+// store.dispatch(fetchPosts('reactjs'))
+
+export function fetchPosts(subreddit) {
+  // Thunk middleware knows how to handle functions.
+  // It passes the dispatch method as an argument to the function,
+  // thus making it able to dispatch actions itself.
+
+  return dispatch => {
+    // First dispatch: the app state is updated to inform
+    // that the API call is starting.
+
+    dispatch(requestPosts(subreddit))
+
+    // The function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method.
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+      .then(
+        response => response.json(),
+        // Do not use catch, because that will also catch
+        // any errors in the dispatch and resulting render,
+        // causing a loop of 'Unexpected batch number' errors.
+        // https://github.com/facebook/react/issues/6895
+        error => console.log('An error occured.', error)
+      )
+      .then(json =>
+        // We can dispatch many times!
+        // Here, we update the app state with the results of the API call.
+
+        dispatch(receivePosts(subreddit, json)))
+  }
+}
+
+function shouldFetchPosts(state, subreddit) {
+  const posts = state.postsBySubreddit[subreddit]
+  if (!posts) {
+    return true
+  } else if (posts.isFetching) {
+    return false
+  } else {
+    return posts.didInvalidate
+  }
+}
+
+export function fetchPostsIfNeeded(subreddit) {
+  // Note that the function also receives getState()
+  // which lets you choose what to dispatch next.
+
+  // This is useful for avoiding a network request if
+  // a cached value is already available.
+
+  return (dispatch, getState) => {
+    if (shouldFetchPosts(getState(), subreddit)) {
+      // Dispatch a thunk from thunk!
+      return dispatch(fetchPosts(subreddit))
+    } else {
+      // Let the calling code know there's nothing to wait for.
+      return Promise.resolve()
+    }
   }
 }
