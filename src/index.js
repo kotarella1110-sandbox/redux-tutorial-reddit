947a25d2a8295fea7275ffc8f15029081a542ec8
KotaroSugawara <KoSugawara@mx1.wiseman.co.jp>
2017-11-13 18:50:14 +0900

    Added containers and components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
+import App from './containers/App'
 
 const loggerMiddleware = createLogger()
 
@@ -11,4 +15,11 @@ const store = createStore(
     thunkMiddleware, // lets us dispatch() functions
     loggerMiddleware // neat middleware that logs actions
   )
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 )
